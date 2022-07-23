d701a22aaffd45c75c18c5e5358d3ec3669ec242
Sanyi81 <skyharts2002@gmail.com>
2022-07-23 16:48:05 +0300

    add js hw1

---
+++ main.js
@@ -0,0 +1,78 @@
+//N1 ------------------
+
+let a = 'hello';
+let b = 'owu';
+let c = 'com';
+let d = 'ua';
+let e = 1;
+let f = 10;
+let g = -999;
+let h = 123;
+let i = 3.14;
+let j = 2.7;
+let k = 16;
+let l = true;
+let m = false;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+console.log(l);
+console.log(m);
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);
+document.write(f);
+document.write(g);
+document.write(h);
+document.write(i);
+document.write(j);
+document.write(k);
+document.write(l);
+document.write(m);
+alert(a);
+alert(b);
+alert(c);
+alert(d);
+alert(e);
+alert(f);
+alert(g);
+alert(h);
+alert(i);
+alert(j);
+alert(k);
+alert(l);
+alert(m);
+
+// N2 -------------------
+
+let firstName = 'Skyhar';
+let middleName = 'Oleksandr';
+let lastName = 'Ivanovich';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person);
+
+// N3 ----------------
+
+let a1 = 100;
+let b1 = '100';
+let c1 = true;
+console.log(typeof a1);
+console.log(typeof b1);
+console.log(typeof c1);
+
+// N4 ----------------
+
+let name = prompt('Your name');
+let surname = prompt('Your surname');
+let age = prompt('Your age');
+alert(`Your name is ${name}, your surname is ${surname}, you are ${age}.`);
+
